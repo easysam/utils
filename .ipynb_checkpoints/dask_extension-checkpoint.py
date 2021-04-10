382fe9ef45265e1f64a819292112c2c8942d99e7
Yizong Wang <wangyizong@bupt.edu.cn>
2021-04-10 20:02:32 +0800

    update .gitignore

--- .ipynb_checkpoints/dask_extension-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from dask.diagnostics import ProgressBar
-
-def pd_reset_index(ddf):
-    ddf = ddf.assign(idx=1)
-    ddf = ddf.set_index(ddf.idx.cumsum() - 1, sorted=True)
-    ddf = ddf.map_partitions(lambda df: df.rename(index = {'idx': None}))
-    ddf = ddf.drop('idx', axis=1)
-    return ddf
